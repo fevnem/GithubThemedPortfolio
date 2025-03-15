aaad0568643c1dd4cc2c98c0bc77dc1dea73fff7
rocket <121660659+therocketor@users.noreply.github.com>
2025-03-15 17:47:24 +0600

    Update vite.config.ts
    Signed-off-by: rocket <121660659+therocketor@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
+/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/GithubThemedPortfolio/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      }
-    }
-  }
 });
